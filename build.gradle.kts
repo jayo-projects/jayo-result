7d9fa24336becee4a315c3a7fa9dd308e721d908
pull-vert <5107035+pull-vert@users.noreply.github.com>
2026-02-25 21:57:03 +0100

    update build.gradle.kts for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.gradle.tooling.GradleConnector
 import kotlin.jvm.optionals.getOrNull
 
 println("Using Gradle version: ${gradle.gradleVersion}")
@@ -65,30 +64,36 @@ publishing {
     }
 
 
-    publications.withType<MavenPublication> {
-        pom {
-            name.set(project.name)
-            description.set("Jayo result is a Java port of the Result<T> type from the Kotlin stdlib")
-            url.set("https://github.com/jayo-projects/jayo-result")
+    publications {
+        create<MavenPublication>("Maven") {
+            from(components["java"])
+        }
 
-            licenses {
-                license {
-                    name.set("Apache-2.0")
-                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+        withType<MavenPublication> {
+            pom {
+                name.set(project.name)
+                description.set("Jayo result is a Java port of the Result<T> type from the Kotlin stdlib")
+                url.set("https://github.com/jayo-projects/jayo-result")
+
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
                 }
-            }
 
-            developers {
-                developer {
-                    name.set("pull-vert")
-                    url.set("https://github.com/pull-vert")
+                developers {
+                    developer {
+                        name.set("pull-vert")
+                        url.set("https://github.com/pull-vert")
+                    }
                 }
-            }
 
-            scm {
-                connection.set("scm:git@github.com/jayo-projects/jayo-result")
-                developerConnection.set("scm:git@github.com/jayo-projects/jayo-result.git")
-                url.set("https://github.com/jayo-projects/jayo-result.git")
+                scm {
+                    connection.set("scm:git@github.com/jayo-projects/jayo-result")
+                    developerConnection.set("scm:git@github.com/jayo-projects/jayo-result.git")
+                    url.set("https://github.com/jayo-projects/jayo-result.git")
+                }
             }
         }
     }
@@ -98,32 +103,6 @@ signing {
     // Require signing.keyId, signing.password and signing.secretKeyRingFile
     sign(publishing.publications)
 }
-
-// workaround : https://github.com/researchgate/gradle-release/issues/304#issuecomment-1083692649
-configure(listOf(tasks.release, tasks.runBuildTasks)) {
-    configure {
-        actions.clear()
-        doLast {
-            GradleConnector
-                .newConnector()
-                .forProjectDirectory(layout.projectDirectory.asFile)
-                .connect()
-                .use { projectConnection ->
-                    val buildLauncher = projectConnection
-                        .newBuild()
-                        .forTasks(*tasks.toTypedArray())
-                        .setStandardInput(System.`in`)
-                        .setStandardOutput(System.out)
-                        .setStandardError(System.err)
-                    gradle.startParameter.excludedTaskNames.forEach {
-                        buildLauncher.addArguments("-x", it)
-                    }
-                    buildLauncher.run()
-                }
-        }
-    }
-}
-
 
 // when the Gradle version changes:
 // -> execute ./gradlew wrapper, then remove .gradle directory, then execute ./gradlew wrapper again
